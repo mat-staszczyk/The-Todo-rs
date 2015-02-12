37040ae361dec984edf7bee64b2d80f808346e8a
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-02-12 08:42:21 +0100

    setting up

---
+++ server.js
@@ -0,0 +1,7 @@
+// set up
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methosOverride = require('method-override');
