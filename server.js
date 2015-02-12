15a58462ff71d37bee0f612a6fd1ca9a3044a289
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-02-12 08:50:03 +0100

    app configuration

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methosOverride = require('method-override');
+
+// config
+mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev')); // log requests to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+// listen
+app.listen(8080);
+console.log("App listening on port 8080");
