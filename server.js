a257311ccbd9bd4beebbf1a1d46c61fd052958a2
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-02-17 10:23:19 +0100

    Create todo function added

--- server.js
+++ server.js
@@ -36,3 +36,21 @@ console.log("App listening on port 8080");
         res.json(todos);
       });
     });
+
+    // create todo and send back all todos after creation
+    app.post('/api/todos', function(req,res) {
+
+      Todo.create({
+        text: req.body.text,
+        done: false
+      }, function(err, todo) {
+        if (err)
+          res.send(err);
+
+        Todo.find(function(err, todos) {
+          if (err)
+            res.send(err)
+          res.json(todos);
+          });
+        });
+      });
