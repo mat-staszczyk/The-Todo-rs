68748b734dbf7fdd03c72fb389127039a9c6e065
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-02-18 15:51:42 +0100

    'delete a todo' function added

--- server.js
+++ server.js
@@ -54,3 +54,21 @@ console.log("App listening on port 8080");
           });
         });
       });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+
+       Todo.remove({
+           _id : req.params.todo_id
+       }, function(err, todo) {
+           if (err)
+               res.send(err);
+
+           // get and return all the todos after you create another
+           Todo.find(function(err, todos) {
+               if (err)
+                   res.send(err)
+               res.json(todos);
+           });
+       });
+    });
