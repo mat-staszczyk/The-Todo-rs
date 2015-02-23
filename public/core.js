cd4f5c3e9b971061b7ca69f99eccf7fcefdb0e21
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-02-23 08:54:40 +0100

    get and show all todos on page load

--- public/core.js
+++ public/core.js
@@ -3,4 +3,12 @@ var scotchTodo = angular.module('scotchTodo', []);
 function mainController($scope, $http) {
     $scope.formData = {};
 
+    $http.get('/api/todos')
+      .success(function(data) {
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
 }
