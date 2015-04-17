cf07de3ca51412a91984408a6d95867d2906aa67
sleaz0id <kwiatkiwoknie@gmail.com>
2015-04-17 17:21:42 +0200

    core.js comments added

--- public/core.js
+++ public/core.js
@@ -3,36 +3,39 @@ var scotchTodo = angular.module('scotchTodo', []);
 function mainController($scope, $http) {
     $scope.formData = {};
 
+    // when landing on the page, get all todos and show them
     $http.get('/api/todos')
-      .success(function(data) {
-        $scope.todos = data;
-        console.log(data);
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
 
-      $scope.createTodo = function() {
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
         $http.post('/api/todos', $scope.formData)
-          .success(function(data) {
-            $scope.formData = {};
-            $scope.todos = data;
-            console.log(data);
-          })
-          .error(function(data {
-            console.log('Error: ' + data);
-          });
-      };
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
 
-      $scope.deleteTodo = function(id) {
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
         $http.delete('/api/todos/' + id)
-          .success(function(data) {
-            $scope.todos = data;
-            console.log(data);
-          })
-          .error(function(data) {
-            console.log('Error: ' + data);
-          });
-      };
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
 
 }
