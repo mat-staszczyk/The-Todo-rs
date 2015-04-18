c6372f181ea1351f1c999e1c9776d9c4cd1d71a3
sleaz0id <kwiatkiwoknie@gmail.com>
2015-04-18 15:50:16 +0200

    server.js code fixed

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // set up
-var express = require('express');
-var app = express();
+var express  = require('express');
+var app      = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // config
-mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu');
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+
 app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev')); // log requests to the console
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
